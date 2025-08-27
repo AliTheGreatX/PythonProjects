05db94ee1d72f78b92df0fe26864a43056ac698e
Dark18x <ali.jessmm@gmail.com>
2025-08-27 22:45:02 +0300

    not working yet

--- PasswordGenerator/passwordgenerator.py
+++ PasswordGenerator/passwordgenerator.py
@@ -4,8 +4,9 @@ import random
 #
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # Get User Customization
-pass_length = input("Enter the Length of the password 8-50")
-
+pass_length = input("Enter the Length of the password 8-50 \n")
+while(50< int(pass_length) <8 and pass_length.isnumeric == False):
+    input("Make Sure the Length of the password is between 8-50 \n")
 # Validation
 
 def GetNewPassword():
