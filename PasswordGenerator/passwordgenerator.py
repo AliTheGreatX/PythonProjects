448d5961670e9d0ec4ec22424565e9da58485c22
Dark18x <ali.jessmm@gmail.com>
2025-08-28 21:36:33 +0300

    better performance

--- PasswordGenerator/passwordgenerator.py
+++ PasswordGenerator/passwordgenerator.py
@@ -1,16 +1,17 @@
 import random
+import string
 # I used this password generator as a reference/inspiration: https://www.lastpass.com/features/password-generator
-
-# Decleare sources
-uAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lAlphabet = "abcdefghijklmnopqrstuvwxyz"
-numbers = "123456789"
-symbols = "!@#$%^&*()_+-=~`|?/><.,:;'" 
-
 
 # Get User Customization
 pass_length = input("Enter the Length of the password 8-50 \n")
-
+hasSymbols = str.lower(input("Does it has symbols?? y/n\n"))
+if(hasSymbols != "y" and hasSymbols !="n"): raise ValueError("Sorry it shold be yes or no input")
+hasNumbers=  str.lower(input("Does it has Numbers?? y/n\n"))
+if(hasNumbers != "y" and hasNumbers !="n"): raise ValueError("Sorry it shold be yes or no input")
+hasLowerCase=  str.lower(input("Does it has LowerCase?? y/n\n"))
+if(hasLowerCase != "y" and hasLowerCase !="n"): raise ValueError("Sorry it shold be yes or no input")
+hasUpperCase=  str.lower(input("Does it has UpperCase?? y/n\n"))
+if(hasUpperCase != "y" and hasUpperCase !="n"): raise ValueError("Sorry it shold be yes or no input")
 # Validation
 while(50< int(pass_length) or int(pass_length) < 8 or pass_length.isnumeric == False):
  pass_length = input("Make Sure the Length of the password is between 8-50 \n")
@@ -20,10 +21,17 @@ pass_length = int(pass_length)
 def GetNewPassword():
     my_password = ""
     while len(my_password)< pass_length:
-        which = random.randint(1,4)
-        if():
-         my_password+= uAlphabet[random.randint(0,len(my_password))]
-    return my_password;
+        allCharacters = ""
+        if(hasUpperCase):
+            allCharacters += string.ascii_uppercase
+        if(hasLowerCase):
+            allCharacters += string.ascii_lowercase
+        if(hasSymbols):
+            allCharacters += string.punctuation
+        if(hasNumbers):
+            allCharacters += "123456789"
+        my_password+= allCharacters[random.randint(0,len(allCharacters))]
+    return my_password
 GetNewPassword()
 # Output...
 print("Your new password is \n" +GetNewPassword())
