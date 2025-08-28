7f3ad5db18120a80fb3433a694cd3e0167d4d976
Dark18x <ali.jessmm@gmail.com>
2025-08-28 21:21:00 +0300

    old school passowrd generator

--- PasswordGenerator/passwordgenerator.py
+++ PasswordGenerator/passwordgenerator.py
@@ -1,15 +1,29 @@
 import random
-# I used this password generator as a reference: https://www.lastpass.com/features/password-generator
+# I used this password generator as a reference/inspiration: https://www.lastpass.com/features/password-generator
 
-#
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# Decleare sources
+uAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lAlphabet = "abcdefghijklmnopqrstuvwxyz"
+numbers = "123456789"
+symbols = "!@#$%^&*()_+-=~`|?/><.,:;'" 
+
+
 # Get User Customization
 pass_length = input("Enter the Length of the password 8-50 \n")
-while(50< int(pass_length) <8 and pass_length.isnumeric == False):
-    input("Make Sure the Length of the password is between 8-50 \n")
-# Validation
 
+# Validation
+while(50< int(pass_length) or int(pass_length) < 8 or pass_length.isnumeric == False):
+ pass_length = input("Make Sure the Length of the password is between 8-50 \n")
+
+pass_length = int(pass_length)
+# Password Generation Proccess
 def GetNewPassword():
     my_password = ""
-    while len(my_password) < pass_length:
-        random.randint()
+    while len(my_password)< pass_length:
+        which = random.randint(1,4)
+        if():
+         my_password+= uAlphabet[random.randint(0,len(my_password))]
+    return my_password;
+GetNewPassword()
+# Output...
+print("Your new password is \n" +GetNewPassword())
