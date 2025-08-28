e590170639086dbc861d687533c28fcb1ca99797
Dark18x <ali.jessmm@gmail.com>
2025-08-28 22:00:18 +0300

    improvement

--- PasswordGenerator/passwordgenerator.py
+++ PasswordGenerator/passwordgenerator.py
@@ -4,6 +4,17 @@ import string
 
 # Get User Customization
 pass_length = input("Enter the Length of the password 8-50 \n")
+
+# Validation
+while(50< int(pass_length) or int(pass_length) < 8 or pass_length.isnumeric == False):
+ pass_length = input("Make Sure the Length of the password is between 8-50 \n")
+
+# Check if the answer is non numeric value
+# if(pass_length.isnumeric == False):
+#     raise ValueError("Make sure it's a number!")
+# change it to an integer
+pass_length = int(pass_length)
+
 hasSymbols = str.lower(input("Does it has symbols?? y/n\n"))
 if(hasSymbols != "y" and hasSymbols !="n"): raise ValueError("Sorry it shold be yes or no input")
 hasNumbers=  str.lower(input("Does it has Numbers?? y/n\n"))
@@ -12,25 +23,20 @@ hasLowerCase=  str.lower(input("Does it has LowerCase?? y/n\n"))
 if(hasLowerCase != "y" and hasLowerCase !="n"): raise ValueError("Sorry it shold be yes or no input")
 hasUpperCase=  str.lower(input("Does it has UpperCase?? y/n\n"))
 if(hasUpperCase != "y" and hasUpperCase !="n"): raise ValueError("Sorry it shold be yes or no input")
-# Validation
-while(50< int(pass_length) or int(pass_length) < 8 or pass_length.isnumeric == False):
- pass_length = input("Make Sure the Length of the password is between 8-50 \n")
-
-pass_length = int(pass_length)
 # Password Generation Proccess
 def GetNewPassword():
     my_password = ""
+    nmbers = "0123456789"
     while len(my_password)< pass_length:
-        allCharacters = ""
-        if(hasUpperCase):
-            allCharacters += string.ascii_uppercase
-        if(hasLowerCase):
-            allCharacters += string.ascii_lowercase
-        if(hasSymbols):
-            allCharacters += string.punctuation
-        if(hasNumbers):
-            allCharacters += "123456789"
-        my_password+= allCharacters[random.randint(0,len(allCharacters))]
+        which = random.randint(1,4)
+        if(hasUpperCase == "y" and which == 1):
+         my_password+= string.ascii_uppercase[random.randint(0,len(string.ascii_uppercase))]
+        if(hasLowerCase == "y"and which == 2):
+         my_password+= string.ascii_lowercase[random.randint(0,len(string.ascii_lowercase))]
+        if(hasSymbols == "y"and which ==3):
+         my_password+= string.punctuation[random.randint(0,len(string.punctuation))]
+        if(hasNumbers == "y"and which == 4):
+         my_password+= nmbers[random.randint(0,len(nmbers))]
     return my_password
 GetNewPassword()
 # Output...
